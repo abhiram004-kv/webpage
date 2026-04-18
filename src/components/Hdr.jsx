495f76a61b6d13742dae67f2f41a155634170169
Abhiram <abhiramkv004@gmail.com>
2026-04-18 12:19:42 +0530

    third1 commit

--- src/components/Hdr.jsx
+++ src/components/Hdr.jsx
@@ -5,7 +5,7 @@ function Hdr() {
   return (
     <div>
       <header>
-        <h1>Welcome to Game Master</h1>
+        <h1>Welcome to Game Center</h1>
         <p>Discover and explore a world of exciting games. Whether you're looking for action, puzzles, or strategy, Game Master has something for everyone.</p>
       </header>
     </div>
